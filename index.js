4fdfc92a4bb9efab8323701540a5f3f5c3e140fc
lumberlark <slark20@gmail.com>
2023-11-01 22:25:17 -0500

    This is the final project in its fullest. The weather app needs to be centered, otherwise- initialy a second API for real-time Launches would have been built next to the weather application, however due to NASA turning off weather sensors, this project is null. A great example however for a practical weather app. please feel free to clone - also noted - the JS will not be up to date , as the API is not in use. Thank you.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const API_KEY = 'zKsFBo0gdGkXugCdyrr6udHxxkTmlziuP6VpxdYh';
+const API_URL = `https://api.nasa.gov/insight_weather/?api_key=zKsFBo0gdGkXugCdyrr6udHxxkTmlziuP6VpxdYh&feedtype=json&ver=1.0`;
+
 /* To move slider up and down for previous weather*/
 
     const previousWeatherToggle = document.querySelector('.show-previous-weather');
@@ -5,4 +8,32 @@
   
     previousWeatherToggle.addEventListener('click', () => {
       previousWeather.classList.toggle('show-weather');
-    });
+    })
+
+    getWeather()
+    
+    function getWeather() {
+      fetch(API_URL)
+      .then(res => res.json())
+      .then(data => {
+        const {
+          sol_keys,
+          validity_checks,
+          ...solData
+        } = data
+       const temp = Object.entries(solData).map(([sol,data]) => {
+        return {
+          sol: sol,
+          maxTemp: data.AT.mx,
+          minTemp: data.AT.mn,
+          windSpeed: data.hws.av,
+          windDirectionDegrees: data.WD.most_common.compass_degrees,
+          windDirectionCardinal: data.WD.most_common.compass_point,
+          date: new Date(data.First_UTC)
+        }
+       })
+       console.log(temp)
+      })
+    }
+    
+ 
